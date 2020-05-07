7b77db4e1ec75aa11e17e090809dea4427f7b898
ziyad <38766552+ziyadalruwashid@users.noreply.github.com>
2020-05-07 16:05:03 +0300

    Add files via upload
    simple classes for making javadoc HW

---
+++ Addrese.java
@@ -0,0 +1,7 @@
+class Address implements Cloneable
+{
+    public String street;
+    public String city;
+    public String state;
+    public String zipCode;
+}
---
+++ Employee.java
@@ -0,0 +1,66 @@
+public class Employee
+{
+    private String lastName;
+    private String firstName;
+    private Double salary;
+    /** Represents the employee’s address.
+     */
+    public Address address;
+    /** Creates an employee with the specified name.
+     * @param lastName The employee’s last name.
+     * @param firstName The employee’s first name.
+     */
+    public Employee(String lastName, String firstName)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = new Address();
+    }
+    /** Gets the employee’s last name.
+     * @return A string representing the employee’s last
+     *     name.
+     */
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+    /** Sets the employee’s last name.
+     * @param lastName A String containing the employee’s
+     *     last name.
+     */
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    /** Gets the employee’s first name.
+     * @return A string representing the employee’s first
+     *     name.
+     */
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+    /** Sets the employee’s first name.
+     * @param firstName A String containing the
+     *     employee’s first name.
+     */
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    /** Gets the employee’s salary.
+     * @return A double representing the employee’s salary.
+     */
+    public double getSalary()
+    {
+        return this.salary;
+    }
+    /** Sets the employee’s salary.
+     * @param lastName A double containing the employee’s
+     *     salary.
+     */
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+}
